4b47e1b4404431889ed9ae6bf1363083d8796cb2
Julius Kricheldorff <julius@kricheldorff.de>
2025-04-01 09:48:57 +0000

    added installation of packages

--- database/intit_db.sh
+++ database/intit_db.sh
@@ -4,6 +4,8 @@ set -e  # Exit immediately if a command exits with a non-zero status
 # Print each command for debugging
 set -x
 
+
+
 echo "Starting TimescaleDB setup process"
 
 # 1. Start Docker containers from the docker-compose file
@@ -16,8 +18,17 @@ sleep 20  # Simple approach
 
 echo "TimescaleDB is ready!"
 
+echo "Install python packages"
+
+# install necessary python packages using pip
+pip install -r requirements.txt
+
+echo "Packages are installed"
+
 echo "Set-up timescale DB databse schema"
 python3 db_setup.py
 
 echo "Retrieve historical data to be loaded into DB"
 python3 hist_ret.py
+
+echo "Setup complete! Your TimescaleDB is ready to use."
