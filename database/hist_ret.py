6bf03210ad60a388942ac70e3991fa207bee9f87
Julius Kricheldorff <julius@kricheldorff.de>
2025-04-01 14:36:04 +0000

    Added variable names to retrieval functions and finished first draft of data retrieval script

--- database/hist_ret.py
+++ database/hist_ret.py
@@ -33,3 +33,99 @@ conn = psycopg2.connect(database="crypto_db",
 # next create a cursor
 cur = conn.cursor()
 
+
+
+# insert the primary keys currency
+cur.execute("""
+INSERT INTO Currency_ID (Currency_name)
+SELECT %s
+WHERE NOT EXISTS (
+    SELECT 1 FROM Currency_ID 
+    WHERE Currency_name = %s
+);
+""", (ret.currency, ret.currency))
+
+# insert the primary keys coin
+cur.execute("""
+INSERT INTO Crypto_ID (Crypto_name)
+SELECT %s
+WHERE NOT EXISTS (
+    SELECT 1 FROM Crypto_ID 
+    WHERE Crypto_name = %s
+);""", (ret.coin, ret.coin))
+
+# insert the primary keys interval
+cur.execute("""INSERT INTO Interval_ID (Interval_name)
+SELECT %s
+WHERE NOT EXISTS (
+    SELECT 1 FROM Interval_ID 
+    WHERE Interval_name = %s
+);""", (ret.interval, ret.interval))
+
+#commit changes
+conn.commit()
+
+for row in data:
+    # first drop the last index, that is garbage and the first which goes in another table:
+    dat = row[1:-1]
+
+    # transform time ID to standard datetime format
+    time = ret.unix_to_datetime(row[0])
+
+
+    # insert the primary key time
+    cur.execute("""INSERT INTO Time_ID (Time_stamp)
+    SELECT %s
+    WHERE NOT EXISTS (
+        SELECT 1 FROM Time_ID
+        WHERE Time_stamp = %s
+    );""", (time, time))
+
+    #commit changes
+    conn.commit()
+
+    # next setup the list we will put in the main table by retrieving the relevant variables
+    cur.execute("""SELECT Time_ID 
+                FROM Time_ID
+                WHERE Time_stamp = %s);""", time)
+    
+    # get id
+    time_id = cur.fetchone()
+
+    # same for interval
+    cur.execute("""SELECT Interval_ID 
+                FROM Interval_ID
+                WHERE Interval_name = %s);""", ret.interval)
+    
+    # get id
+    interval_id = cur.fetchone()
+
+    # same for crypto
+    cur.execute("""SELECT Crypto_ID 
+                FROM Crypto_ID
+                WHERE Crypto_name = %s);""", ret.coin)
+    
+    # get id
+    Crypto_id = cur.fetchone()
+
+    # same for currency
+    cur.execute("""SELECT Currency_ID 
+                FROM Currency_ID
+                WHERE Currency_name = %s);""", ret.currency)
+    
+    # get id
+    Currency_id = cur.fetchone()
+
+    # put the full row together
+    new_list = [time_id, Crypto_id , Currency_id, interval_id] + dat
+
+    # insert in to the main database
+    cur.execute("""INSERT INTO Main_Tb (Time_ID, Crypto_ID, Currency_ID, 
+                Interval_ID, Open_price, Close_price, High_price, Low_price,
+                Volume, Close_time, Nr_trades, Quote_asset_volume, TB_based_asset_volume,
+                TB_quote_asset_volume)
+                VALUES (%s);
+                """, new_list)
+    
+    #commit changes
+    conn.commit()
