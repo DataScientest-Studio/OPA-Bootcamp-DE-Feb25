db3485381b1a9ae1f6a02f8d71675d51e792143d
Julius Kricheldorff <julius@kricheldorff.de>
2025-04-02 13:43:54 +0000

    tables are deleted before initialization, removed time_id and currency_id tables as redundant

--- database/db_setup.py
+++ database/db_setup.py
@@ -15,6 +15,12 @@ conn = psycopg2.connect(database="crypto_db",
 cur = conn.cursor()
 
 print("---------------------------------")
+print("-- Delete any previously existing table witht the same name")
+print("---------------------------------")
+cur.execute("""DROP TABLE IF EXISTS Crypto_ID, Interval_ID, Main_Tb;""")
+conn.commit()
+
+print("---------------------------------")
 print("-- Create Crypto_ID table")
 print("---------------------------------")
 cur.execute("""CREATE TABLE IF NOT EXISTS Crypto_ID(
@@ -22,13 +28,6 @@ cur.execute("""CREATE TABLE IF NOT EXISTS Crypto_ID(
     Crypto_name varchar(200) NOT NULL
 );""")
 
-print("---------------------------------")
-print("-- Create Currency_ID table")
-print("---------------------------------")
-cur.execute("""CREATE TABLE IF NOT EXISTS Currency_ID(
-    Currency_ID serial PRIMARY KEY,
-    Currency_name varchar(200) NOT NULL
-);""")
 
 print("---------------------------------")
 print("-- Create Interval_ID table")
@@ -38,29 +37,22 @@ cur.execute("""CREATE TABLE IF NOT EXISTS Interval_ID(
     Interval_name varchar(200) NOT NULL
 );""")
 
-print("---------------------------------")
-print("-- Create Time_ID table")
-print("---------------------------------")
-cur.execute("""CREATE TABLE IF NOT EXISTS Time_ID(
-    Time_ID serial PRIMARY KEY,
-    Time_stamp date NOT NULL
-    
-);""")
+
 
 print("---------------------------------")
 print("-- Create Main_Tb table")
 print("---------------------------------")
 cur.execute("""CREATE TABLE IF NOT EXISTS Main_Tb(
-    Time_ID INTEGER REFERENCES Time_ID(Time_ID),
     Crypto_ID INTEGER REFERENCES Crypto_ID(Crypto_ID),
-    Currency_ID INTEGER REFERENCES Currency_ID(Currency_ID),
     Interval_ID INTEGER REFERENCES Interval_ID(Interval_ID),
+    Currency_name varchar(200) NOT NULL,
+    Open_time DATETIME,
     Open_price float NOT NULL,
     Close_price float NOT NULL,
     High_price float NOT NULL,
     Low_price float NOT NULL,
     Volume float NOT NULL,
-    Close_time date NOT NULL,
+    Close_time DATETIME,
     Nr_trades int NOT NULL,
     Quote_asset_volume float NOT NULL,
     TB_based_asset_volume float NOT NULL,
